3261c814e9c4b2cffb93c815acabc4e847561ca0
1VinceP <vpalmergraphics@gmail.com>
2018-09-20 16:28:41 -0600

    updated about

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -9,7 +9,7 @@ function About() {
             <div className='about-header'>About Me</div>
 
             <p className='about-info'>
-                I am a hard worker that is always excited to learn new things. From sports to musical instruments and hiking to web development, I have many interests and talents that keep me on my toes and ready for the next new thing. I enjoy working in a group setting, and am comfortable with both giving and receiving direction. However, I also function well on my own and have driven myself to learn - and subsequently accomplish - great feats I never would have thought possible at the start of the project. This drive and zeal is what has led me to be the developer that I am now, despite my relatively short time in the field, and I'm excited to see where it takes me from here.
+                I am a hard worker that is always excited to learn new things. From sports to musical instruments and hiking to web development, I have many interests and talents that keep me on my toes and ready for the next new thing. I enjoy working in a group setting, and am comfortable with both giving and receiving direction. However, I also function well on my own and have driven myself to learn - and subsequently accomplish - great feats I never would have thought possible at the start of the project. This drive and zeal is what has led me to be the developer that I am now, and I'm excited to see where it takes me from here.
             </p>
         </div>
     )
